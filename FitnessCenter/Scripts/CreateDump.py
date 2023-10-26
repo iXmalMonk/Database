8ce05b1b9b49795ebd0198f554922ef16f0f550f
iXmalMonk <ixmalmonk@gmail.com>
2023-10-26 10:12:37 +0300

    Variable renamed

--- FitnessCenter/Scripts/CreateDump.py
+++ FitnessCenter/Scripts/CreateDump.py
@@ -5,8 +5,8 @@ import subprocess
 with open("..\\FitnessCenter\\Scripts\\Settings.json") as file:
     settings = json.load(file)
 
-dump_file = "..\\FitnessCenter\\Dump\\" + "dump_" + datetime.now().strftime("%Y_%m_%d_%H_%M_%S") + ".dump"
+file = "..\\FitnessCenter\\Dump\\" + "dump_" + datetime.now().strftime("%Y_%m_%d_%H_%M_%S") + ".dump"
 
-command = f'pg_dump --dbname="{settings["database"]}" --host="{settings["host"]}" --username="{settings["user"]}" --format=custom --file="{dump_file}"'
+command = f'pg_dump --dbname="{settings["database"]}" --host="{settings["host"]}" --username="{settings["user"]}" --format=custom --file="{file}"'
 
 subprocess.run(command, shell=True)
