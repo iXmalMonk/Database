8f50b16511fde36c3a6c939bae3be3615664308e
iXmalMonk <ixmalmonk@gmail.com>
2023-11-21 21:02:43 +0300

    Update scripts

--- FitnessCenter/Scripts/Users/CreateUsers.sql
+++ FitnessCenter/Scripts/Users/CreateUsers.sql
@@ -1,9 +1,13 @@
 create user user1 with password 'password1';
 create user user2 with password 'password2';
+create user user3 with password 'password3';
 create group group1;
 
 grant usage on schema fc to user1;
 grant select on fc.body_mass_index to user1;
+grant select on fc.visit to user2;
 grant usage on schema fc to group group1;
-grant insert, update, delete on fc.client to group1;
+grant select on fc.client to group1;
+grant insert, update, delete on fc.group to group1;
 grant group1 to user2;
+grant group1 to user3;
--- FitnessCenter/Scripts/Users/DropUsers.sql
+++ FitnessCenter/Scripts/Users/DropUsers.sql
@@ -1,7 +1,9 @@
 drop owned by group1 cascade;
 drop owned by user1 cascade;
 drop owned by user2 cascade;
+drop owned by user3 cascade;
 
 drop group if exists group1;
 drop user if exists user1;
 drop user if exists user2;
+drop user if exists user3;
