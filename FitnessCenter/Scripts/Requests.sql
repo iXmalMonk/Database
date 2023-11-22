81eaeff6ee5557777c0fa76351bef99ccf08cb2e
iXmalMonk <ixmalmonk@gmail.com>
2023-11-22 13:00:35 +0300

    Update requests script

--- FitnessCenter/Scripts/Requests.sql
+++ FitnessCenter/Scripts/Requests.sql
@@ -3,8 +3,8 @@
 --select distinct weight from fc.body_mass_index where height >= 1.88;
 
 --select * from fc.client where full_name like 'Ильин Александр Георгиевич';
---select * from fc.body_mass_index where height in (1.6, 1.7);
---select * from fc.body_mass_index where height between 1.6 and 1.8;
+--select * from fc.body_mass_index where height in (1.5, 1.9);
+--select * from fc.body_mass_index where height between 1.5 and 1.75;
 --select * from fc.visit where client_id is null;
 --select * from fc.visit where trainer_id is not null;
 --select * from fc.body_mass_index order by height asc;
@@ -13,5 +13,11 @@
 --select count(height) from fc.body_mass_index;
 --select sum(height) from fc.body_mass_index;
 
---select height, count(*) from fc.body_mass_index where weight > 70 group by height;
+--select height, count(*) from fc.body_mass_index where weight > 50 group by height;
+
+--select * from fc.body_mass_index bmi inner join fc.client c on bmi.id = c.body_mass_index_id;
+
+--select * from fc.body_mass_index bmi left join fc.client c on bmi.id = c.body_mass_index_id where c.body_mass_index_id >= 15;
+
+--select c.full_name, bmi.weight, bmi.height from fc.client c inner join fc.body_mass_index bmi on c.body_mass_index_id = bmi.id where bmi.bms > 25;
 
